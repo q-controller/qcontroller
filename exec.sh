39a3e7f5b8df7a79d39ff86b029e5aa31aa3eb92
Nikita Vakula <nikita.vakula.dev@gmail.com>
2025-08-04 20:17:30 +0200

    Add script to build project inside a container
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

---
+++ exec.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+DOCKERFILE=$(mktemp)
+
+cleanup() {
+    local exit_code=$?
+    exit "$exit_code"
+}
+
+BUILD_DIR=${script_dir}/build
+GEN_DIR=${script_dir}/src/generated
+CACHE_DIR=/tmp/qcontroller-cache
+
+mkdir -p ${CACHE_DIR}/.go
+mkdir -p ${CACHE_DIR}/.go-mod-cache
+mkdir -p ${CACHE_DIR}/.buf
+
+docker run --rm -it -v "${script_dir}:${script_dir}" \
+    -v "${CACHE_DIR}/.go:${CACHE_DIR}/.go" \
+    -v "${CACHE_DIR}/.go-mod-cache:${CACHE_DIR}/.go-mod-cache" \
+    -v "${CACHE_DIR}/.buf:${CACHE_DIR}/.buf" \
+    --workdir ${script_dir} \
+    -e BUF_CACHE_DIR=${CACHE_DIR}/.buf \
+    -e GOCACHE=${CACHE_DIR}/.go \
+    -e GOMODCACHE=${CACHE_DIR}/.go-mod-cache \
+    $(docker build -q -t buf --target pre-build . -f Dockerfile --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)) \
+    bash -c "$@"
