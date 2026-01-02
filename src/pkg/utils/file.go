7a86a891e5e332ad25cd015a80b9a099837cebf2
Nikita Vakula <programmistov.programmist@gmail.com>
2026-01-02 10:23:28 +0100

    Fix management of lifecycle of the instance
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/pkg/utils/file.go
+++ src/pkg/utils/file.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"log/slog"
 	"os"
+	"path/filepath"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func CopyFile(src, dst string) error {
@@ -27,4 +32,59 @@ func TouchFile(path string) error {
 	}
 
 	return os.Chtimes(path, now, now)
+}
+
+func WaitForFileCreation(filename string, timeout time.Duration) error {
+	// First check if file already exists
+	if _, err := os.Stat(filename); err == nil {
+		return nil // File already exists
+	}
+
+	dir := filepath.Dir(filename)
+	base := filepath.Base(filename)
+
+	watcher, watcherErr := fsnotify.NewWatcher()
+	if watcherErr != nil {
+		return watcherErr
+	}
+	defer func() {
+		if err := watcher.Close(); err != nil {
+			slog.Error("WaitForFileCreation: failed to close watcher", "error", err)
+		}
+	}()
+
+	if addErr := watcher.Add(dir); addErr != nil {
+		return addErr
+	}
+
+	// Check again after starting the watcher to avoid race condition
+	if _, statErr := os.Stat(filename); statErr == nil {
+		return nil
+	}
+
+	slog.Debug("WaitForFileCreation: starting to watch directory", "directory", dir, "filename", base, "timeout", timeout)
+	deadline := time.After(timeout)
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return fmt.Errorf("watcher closed")
+			}
+			slog.Debug("WaitForFileCreation: received event", "event", event.Op, "name", event.Name)
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				if filepath.Base(event.Name) == base {
+					slog.Debug("WaitForFileCreation: target file created", "name", event.Name)
+					return nil
+				} else {
+					slog.Debug("WaitForFileCreation: different file created", "created", filepath.Base(event.Name), "looking_for", base)
+				}
+			}
+		case _, ok := <-watcher.Errors:
+			if !ok {
+				return fmt.Errorf("watcher error channel closed")
+			}
+		case <-deadline:
+			return fmt.Errorf("timeout waiting for %s", filename)
+		}
+	}
 }
